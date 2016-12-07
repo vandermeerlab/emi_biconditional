e6390e5aab96dbd507df3a6c9dde334bbde31598
Emily Irvine <emilymirvine@gmail.com>
2016-12-07 18:44:08 -0500

    Loading events from neuralynx using PostEvent

--- startup.py
+++ startup.py
@@ -2,6 +2,7 @@ import scipy.io as sio
 
 def load_events(matfile):
     loading_events = sio.loadmat(matfile)
+    print(loading_events['evt_tone_on'])
     events = dict()
     events['start_recording'] = loading_events['evt_start'][0]
     events['stop_recording'] = loading_events['evt_stop'][0]
@@ -17,13 +18,9 @@ def load_events(matfile):
     events['tone_on'] = loading_events['evt_tone_on'][0]
     events['tone_off'] = loading_events['evt_tone_off'][0]
     events['trial1_start'] = loading_events['evt_trial1_start'][0]
-    events['trial1_stop'] = loading_events['evt_trial1_stop'][0]
     events['trial2_start'] = loading_events['evt_trial2_start'][0]
-    events['trial2_stop'] = loading_events['evt_trial2_stop'][0]
     events['trial3_start'] = loading_events['evt_trial3_start'][0]
-    events['trial3_stop'] = loading_events['evt_trial3_stop'][0]
     events['trial4_start'] = loading_events['evt_trial4_start'][0]
-    events['trial4_stop'] = loading_events['evt_trial4_stop'][0]
     events['type'] = loading_events['evt_type'][0]
     events['label'] = []
     for label in loading_events['evt_label'][0]:
