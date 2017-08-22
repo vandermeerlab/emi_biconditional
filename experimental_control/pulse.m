f950f58139020eb36d6e8cce6b9b98cc0ff235c4
Emily Irvine <emilymirvine@gmail.com>
2017-08-22 11:34:54 -0400

    Removing experimental control scripts
    Moved to private repository that also includes task descriptions.

--- experimental_control/pulse.m
+++
@@ -1,5 +0,0 @@
-function pulse(pulse_on_timer, pulse_off_timer, control)
-    NlxSendCommand(['-PostEvent "', control.event, '_on" ', control.ttl, ' ', control.id']);
-    start(pulse_off_timer)
-    start(pulse_on_timer)
-end
--- experimental_control/set_nlx.m
+++
@@ -1,5 +0,0 @@
-function set_nlx(onoff, control)
-    NlxSendCommand(['-PostEvent "', control.event, '_', onoff, '" ', control.ttl, ' ', control.id']);
-    NlxSendCommand([control.command, ' AcqSystem1_0 ', control.port, ' ' , control.pin, ' ', onoff]);
-
-end
